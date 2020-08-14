9805288ab904853b2787a61d3c5e1b1fa7ee6e83
huangyichao0916 <939121836@qq.com>
2020-08-14 20:04:24 +0800

    468. 验证IP地址

---
+++ 468. 验证IP地址/1.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} IP
+ * @return {string}
+ */
+var validIPAddress = function(IP) {
+    let i4 = IP.split('.');
+    let i6 = IP.split(':');
+    if (i4.length === 4) {
+        let isI4 = i4.every(item => /^0$|[1-9]\d{0,2}/.test(item) && item < 256);
+        if (isI4) {
+            return 'IPv4';
+        }else{
+            return 'Neither';
+        }
+    }else if (i6.length === 8) {
+        let isI6 = i6.every(item => /^[a-f0-9]{1,4}$/i.test(item));
+        if (isI6) {
+            return 'IPv6';
+        }else{
+            return 'Neither';
+        }
+    }else{
+        return 'Neither';
+    }
+};
