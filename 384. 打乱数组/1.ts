65f66c9d482da1cc81089e2dabd7a505ab208993
huangyichao0916 <939121836@qq.com>
2020-08-24 19:41:05 +0800

    test

--- 384. 打乱数组/1.ts
+++ 384. 打乱数组/1.ts
@@ -15,6 +15,7 @@ class Solution {
         const len:number = this.nums.length;
         for (let i = 0; i < len - 1; i++) {
             const randomNum:number = Math.floor(Math.random()*(len - i) + i);
+            // const randomNum:number = Math.floor(Math.random()*(len - i - 1) + i + 1);
             [this.nums[i],this.nums[randomNum]] = [this.nums[randomNum],this.nums[i]];
         }
         return this.nums;
@@ -23,7 +24,7 @@ class Solution {
 
 let obj:Solution = new Solution([1,2,3,4,5]);
 let arr = [0,0,0,0,0];
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 1000000; i++) {
     let [a,b,c,d,e] = obj.shuffle();
     let ooo:Array<number> = [a,b,c,d,e];
     for (let j = 0; j < arr.length; j++) {
